1ddbabb4bb8db86b745967c37c36d2e9bff91eb1
randyz0123 <randyzofficial66@gmail.com>
2026-04-26 14:28:13 +0800

    feat: add playable zhouzhou block island threejs prototype

---
+++ app/zhouzhou-block-island/page.tsx
@@ -0,0 +1,12 @@
+export default function ZhouzhouBlockIslandPage() {
+  return (
+    <main style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+      <iframe
+        src="/zhouzhou-block-island.html"
+        title="舟舟 方块岛"
+        style={{ width: '100%', height: '100%', border: 'none' }}
+        allow="fullscreen"
+      />
+    </main>
+  );
+}
